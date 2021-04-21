19160e06ed37c7bac5c2f3661c2740e0a14bd963
sthakur66 <71988949+sthakur66@users.noreply.github.com>
2021-04-20 19:00:57 -0500

    Updated repo with Data Cleanup and Postgres

---
+++ Data Cleanup & Postgres/Queries.sql
@@ -0,0 +1,43 @@
+SELECT * FROM earthquake;
+SELECT * FROM location;
+SELECT * FROM time;
+
+
+
+CREATE TABLE "earthquake" (
+    "id" VARCHAR(50)   NOT NULL,
+    "type" VARCHAR(50)   NOT NULL,
+    "depth" FLOAT   NOT NULL,
+    "mag" FLOAT   NOT NULL,
+    "magType" VARCHAR(20)   NOT NULL,
+    "rootMeanSquare" FLOAT   NOT NULL,
+    "status" VARCHAR(30)   NOT NULL,
+    CONSTRAINT "pk_earthquake" PRIMARY KEY (
+        "id"
+     )
+);
+
+CREATE TABLE "location" (
+    "id" VARCHAR(50)   NOT NULL,
+    "place" VARCHAR(255)   NOT NULL,
+    "latitude" FLOAT   NOT NULL,
+    "longitude" FLOAT   NOT NULL,
+    "locationSource" VARCHAR(20)   NOT NULL,
+    "City" VARCHAR(50)   NOT NULL,
+    "County" VARCHAR(50)   NOT NULL,
+    "State" VARCHAR(50)   NOT NULL,
+    "Country" VARCHAR(50)   NOT NULL
+);
+
+CREATE TABLE "time" (
+    "id" VARCHAR(50)   NOT NULL,
+    "time" timestamp   NOT NULL,
+    "lastUpdDatetime" timestamp   NOT NULL
+);
+
+ALTER TABLE "location" ADD CONSTRAINT "fk_location_id" FOREIGN KEY("id")
+REFERENCES "earthquake" ("id");
+
+ALTER TABLE "time" ADD CONSTRAINT "fk_time_id" FOREIGN KEY("id")
+REFERENCES "earthquake" ("id");
+
