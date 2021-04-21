e09f2b8f6e96458ffcc72d48b3e5b47ca01f5f40
Bao <bthuynh90@gmail.com>
2021-04-20 20:12:47 -0400

    updated files 	modified:   static/js/logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -33,14 +33,9 @@ function createMap(earthquakes) {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   })
 
-  var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-  });
 
   // Create a baseMaps object.
   var baseMaps = {
-    "Street Map": street,
-    "Topographic Map": topo
   };
 
   // Create an overlay object to hold our overlay.
